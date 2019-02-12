44cc8bf8eb7d8042b6593fab6c096be8ba9ef840
Youngsun Kim <yskim041@gmail.com>
2019-02-11 18:16:47 -0800

    fix minor issues, and test run_perception_module

--- src/food_detector/run_perception_module.py
+++ src/food_detector/run_perception_module.py
@@ -1,12 +1,16 @@
 import sys
 import rospy
 import os
-
 import argparse
+
+from visualization_msgs.msg import Marker
+
 from deep_pose_estimators.run_perception_module import run_detection
 from deep_pose_estimators.perception_module import PerceptionModule
+from deep_pose_estimators.marker_manager import MarkerManager
+
 from food_detector import FoodDetector
-from visualization_msgs.msg import Marker
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Run perception module for ada feeding projects")
@@ -15,7 +19,7 @@ if __name__ == '__main__':
 
     rospy.init_node("food_detection")
 
-    if args.demo_type == "spnet_skewer":
+    if args.demo_type == "spnet-skewer":
         import ada_feeding_demo_config as conf
         # TODO: shall we allow other options?
         pose_estimator = FoodDetector(use_spnet=True, use_cuda=True, use_model1=False)
