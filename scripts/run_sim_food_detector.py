8ebf56ec9f89d6d6375973e9d9e962227f6d6f80
Gilwoo Lee <gilwoo301@gmail.com>
2019-02-18 13:04:57 -0800

    Add sim food detector

---
+++ scripts/run_sim_food_detector.py
@@ -0,0 +1,37 @@
+# Example script for running a perception module
+
+from deep_pose_estimators.perception_module import PerceptionModule
+from deep_pose_estimators.marker_manager import MarkerManager
+from deep_pose_estimators.run_perception_module import run_detection
+from food_detector import SimFoodDetector
+import rospy
+
+
+# This script runs a sim food detector which publishes markers for food items
+# on the plate.
+
+# Run in command line a static transform between map to detection frame
+# rosrun tf static_transform_publisher 0.0 0.0 0.0 0.0 0.0 0.0 1.0 map detection_frame 1000
+# You should be able to see marker array in rviz under topic /simulated_pose/marker_array
+if __name__ == "__main__":
+    detection_frame = "camera_color_optical_frame"
+    destination_frame = "map"
+    # Change to Robot Base Link, e.g.:
+    #destination_frame = "j2n6s200_link_base"
+
+    rospy.init_node("sim_food_detector")
+
+    pose_estimator = SimFoodDetector(detection_frame)
+    marker_manager = MarkerManager(count_items=False)
+
+    perception_module = PerceptionModule(
+        pose_estimator=pose_estimator,
+        marker_manager=marker_manager,
+        detection_frame_marker_topic=None,  # Not used since pose estimator is provided.
+        detection_frame=detection_frame,
+        destination_frame=destination_frame,
+        purge_all_markers_per_update=True)
+
+    destination_frame_marker_topic = "deep_pose"
+    frequency = 5
+    run_detection(destination_frame_marker_topic, frequency, perception_module)
---
+++ src/food_detector/sim_food_detector.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from deep_pose_estimators.pose_estimators import PoseEstimator
+from deep_pose_estimators.detected_item import DetectedItem
+
+class SimFoodDetector(PoseEstimator):
+    def __init__(self, frame_id):
+
+        # Pose at which the food is on the plate
+        pose1 = np.array([[-1, 0, 0, 0.30],
+                          [0, 1, 0, -0.25],
+                          [0, 0, -1, 0.25],
+                          [0, 0, 0, 1]])
+        self.item1 = DetectedItem(
+            frame_id=frame_id,
+            marker_namespace="cantaloupe",
+            marker_id=1,
+            db_key="food_item",
+            pose=pose1,
+            detected_time=rospy.Time.now())
+
+        # Pose at which the food is on the plate
+        pose2 = np.array([[-1, 0, 0, 0.25],
+                          [0, 1, 0, -0.29],
+                          [0, 0, -1, 0.25],
+                          [0, 0, 0, 1]])
+        self.item2 = DetectedItem(
+            frame_id=frame_id,
+            marker_namespace="cantaloupe",
+            marker_id=2,
+            db_key="food_item",
+            pose=pose2,
+            detected_time=rospy.Time.now())
+
+    def detect_objects(self):
+        self.item1.detected_time = rospy.Time.now()
+        self.item2.detected_time = rospy.Time.now()
+        return [self.item1, self.item2]
